0242b4eadcb6c372d9b4ba2a2341a317f479e373
Chris Burgess <chris@giantrobot.co.nz>
2017-04-24 17:37:25 +1200

    php7

--- tests/_support/CiviHelper.php
+++ tests/_support/CiviHelper.php
@@ -11,7 +11,7 @@ use \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
 
 class CiviHelper extends \Codeception\Module
 {
-  static protected $civicrm_api3;
+  protected $civicrm_api3;
 
   protected $requiredFields = [
     'api_key',
