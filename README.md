0ee3086f1c00e659be19198501d83104ea0c51b8
Chris Burgess <chris@giantrobot.co.nz>
2017-05-10 13:17:15 -0500

    Add some more docs.

--- README.md
+++ README.md
@@ -30,3 +30,32 @@ Let's get some code generated in here.
 
     codecept bootstrap
 
+### What's in here
+
+`tests/_support/Step/Acceptance/ContributionPage.php`
+
+This is a Codeception Page Helper - it wraps up common functionality for a page (ish). Helper methods like `detectOtherAmountIsRequired`, `etectOtherAmountIsRequired`, `disableWarningForUnsavedChanges`, `fillAmountFields`, `fillCiviEventFields`, `fillCiviContributeFields`, `fillCiviRequiredFields`, `completeTransaction`.
+
+Most of those are fairly explanator by their names.
+
+`completeTransaction` uses a switch statement to branch based on the payment providers. This would be better restructured into separate helpers loaded based on the payment provider. Want to help me understand DI? I'd love to :)
+ 
+`tests/_support/CiviRemoteApi.php`
+
+This is a Codeception Module - it provides additional methods to the Acceptance Tester, in this case the ability to call CiviCRM API remotely via CiviCRM's `extern/rest.php`. If this is loaded, then the acceptance tester (`$I`) will have that method available. The details of CiviCRM API access are retrieved from the suite's config `.yml` (or can be passed in as part of `$params`).
+
+A simple demo of this is available in `tests/functional/CiviRemoteCest.php`.
+
+There are a couple of additional "decoy" helpers, `CiviHelper` and `CiviRemoteAPIHelper`. Ignore these - they are just early efforts and should be removed.
+
+`tests/acceptance/DonationPagesCest.php`
+
+This is an acceptance test which utilises the above Codeception functionality.
+
+`contributionPageProvider` is a dataprovider method which uses CiviRemoteApi to retrieve a list of testable Contribution Pages.
+ 
+`AllDonationPages` is a test which tests all the donation pages; the `@dataprovider contributionPageProvider` instructs this test to run once with each example provided by the dataprovider. 
+
+`AllDonationPagesByExample` demonstrates doing similarly, but uses the `@example` annotation instead to provide test examples.
+
+`DonationPages` is possibly an older example.
--- tests/acceptance/DonationPagesCest.php
+++ tests/acceptance/DonationPagesCest.php
@@ -159,4 +159,128 @@ class DonationPagesCest
             'payment_processor_class_name' => $example['payment_processor_class_name'],
         ]);
     }
+
+    /**
+     * A DataProvider would be better here.
+     * https://github.com/fuzionnz/codeception-example/issues/1
+     *
+     * Then we would separate the form completion logic (should get a single
+     * test case to check out, eg "Donate $1 non-recurring using Auth.NET") from
+     * the logic which obtains the set of cases.
+     *
+     * @param \AcceptanceTester $I
+     *
+     * @group donation
+     */
+    public function DonationPages(\Step\Acceptance\ContributionPage $I)
+    {
+        // DataProvider section.
+        $params = [
+            'entity' => 'ContributionPage',
+            'action' => 'get',
+            'is_active' => 1,
+            // 'id' => 3,
+            //            'options' => [
+            //                'limit' => 1,
+            //            ],
+        ];
+        $pages = $I->CiviRemote($params);
+        $examples = [];
+
+        // Iterate over pages to pick up payment processors.
+        foreach ($pages['values'] as $page) {
+            $example = [
+                'page_id' => $page['id'],
+                'page_title' => $page['title'],
+                'page_url' => "civicrm/contribute/transact?reset=1&id={$page['id']}",
+            ];
+
+            // CiviCRM does not make price set data available to Contribute API?
+            // If we want to test beyond the default price option, we need an
+            // extension adding data via hook_civicrm_apiWrappers() or to
+            // or to retrieve options from the DOM.
+            // @see CRM-20503
+
+            // Check if "amount block" is active, and whether we got back a price
+            // set from the API.
+//            if ($page['amount_block_is_active']) {
+            if (isset($page['min_amount']) && $page['min_amount'] > 0) {
+                $example['other_amount'] = $page['min_amount'];
+            } else {
+                $example['other_amount'] = 1;
+            }
+//            }
+
+            if (isset($page['payment_processor'])) {
+                // If API returned a single value, make it an array.
+                $processor_ids = (is_array($page['payment_processor'])) ?
+                    $page['payment_processor'] : [$page['payment_processor']];
+
+                foreach ($processor_ids as $payment_processor_id) {
+                    $params = [
+                        'entity' => 'PaymentProcessor',
+                        'action' => 'get',
+                        'id' => $payment_processor_id,
+                        'sequential' => true,
+                    ];
+                    $payment_processor = $I->CiviRemote($params);
+                    $payment_processor = $payment_processor['values'][0];
+                    $example['payment_processor_id'] = $payment_processor_id;
+                    $example['payment_processor_class_name'] = $payment_processor['class_name'];
+                    $example['payment_processor_billing_mode'] = $payment_processor['billing_mode'];
+                    $example['payment_processor_is_recur'] = $payment_processor['is_recur'];
+
+                    $params = [
+                        'entity' => 'PaymentProcessorType',
+                        'action' => 'get',
+                        'id' => $payment_processor['payment_processor_type_id'],
+                        'sequential' => 1,
+                    ];
+                    $payment_processor_type = $I->CiviRemote($params);
+                    $payment_processor_type = $payment_processor_type['values'][0];
+                    $example['payment_processor_type_name'] = $payment_processor_type['name'];
+                    $example['payment_processor_type_title'] = $payment_processor_type['title'];
+
+                    $examples[] = $example;
+                    codecept_debug(['$example' => $example]);
+                }
+            }
+        }
+
+        // Alright, we've built a list of contribution pages examples. Let's test them!
+        codecept_debug(['$examples' => $examples]);
+
+        foreach ($examples as $example) {
+            $I->amOnPage($example['page_url']);
+            $I->see($example['page_title']);
+
+            // Where there's no default amount & the other amount is required,
+            // contribute the minimum amount.
+            $I->fillAmountFields($example['other_amount']);
+
+            // Complete the required fields.
+            $I->fillCiviContributeFields();
+
+            $I->completeTransaction([
+                'mode' => 'live',
+                'payment_processor_id' => $example['payment_processor_id'],
+                'payment_processor_class_name' => $example['payment_processor_class_name'],
+            ]);
+
+            // Because we're not calling this method repeatedly, need to reset
+            // session before proceeding.
+        }
+
+        // required_values is the civicrm field names (from civicrm pages)
+        // + any associated with this donation page
+        // + accept a generator of some sort - can be civicrm api get all donation pages
+        // + dynamically match any field marked "required" as populsyttyrf
+
+        // Run a transaction - for each payment processor on the page.
+        // API doesn't reveal processors?! So we will use the default processor for
+        // now. Make it easy to add second test for same page with diff processor?
+
+        // These are fetchable via lookup (use civicrm json/http).
+
+    }
 }
